a641c7bff29686d127d0eed0a0f7bf6ceaa20357
ericmartinezr <eric.martinez@live.cl>
2015-09-10 15:30:48 -0300

    Fixes

---
+++ app.js
@@ -0,0 +1,31 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var bootstrap_1 = require('angular2/bootstrap');
+var angular2_1 = require('angular2/angular2');
+var http_1 = require('angular2/http');
+var router_1 = require('angular2/router');
+var App = (function () {
+    function App() {
+    }
+    App = __decorate([
+        angular2_1.Component({
+            selector: 'app'
+        }),
+        angular2_1.View({
+            template: 'Hello world!'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], App);
+    return App;
+})();
+bootstrap_1.bootstrap(App, [http_1.HTTP_BINDINGS, router_1.ROUTER_BINDINGS]);
