136343f2080bd0bd31959fcc7195c84e86f258ee
ericmartinezr <eric.martinez@live.cl>
2015-09-11 10:09:30 -0300

    Reverted bootstrap to angular2/angular2

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var bootstrap_1 = require('angular2/bootstrap');
 var angular2_1 = require('angular2/angular2');
 var http_1 = require('angular2/http');
 var router_1 = require('angular2/router');
@@ -28,4 +27,4 @@ var App = (function () {
     ], App);
     return App;
 })();
-bootstrap_1.bootstrap(App, [http_1.HTTP_BINDINGS, router_1.ROUTER_BINDINGS]);
+angular2_1.bootstrap(App, [http_1.HTTP_BINDINGS, router_1.ROUTER_BINDINGS]);
--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
 
-import {bootstrap} from 'angular2/bootstrap';
-import {Component, View} from 'angular2/angular2';
+import {Component, View, bootstrap} from 'angular2/angular2';
 import {Http, HTTP_BINDINGS} from 'angular2/http';
 import {ROUTER_BINDINGS} from 'angular2/router';
 
