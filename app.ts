8ba6478cff48d26e65594a595cb5b168ad2af868
ericmartinezr <eric.martinez@live.cl>
2015-10-09 09:51:38 -0300

    Removed @View annotation since is optional

--- app.ts
+++ app.ts
@@ -1,13 +1,9 @@
-/// <reference path="typings/angular2/angular2.d.ts" />
-
-import {Component, View, bootstrap, bind} from 'angular2/angular2';
+import {Component, bootstrap, bind} from 'angular2/angular2';
 import {Http, HTTP_BINDINGS} from 'angular2/http';
 import {ROUTER_BINDINGS, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
 
 @Component({
-	selector : 'app'
-})
-@View({
+	selector : 'app',
 	template : 'Hello world!'
 })
 class App {
