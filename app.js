39259863aca1ddb7eb07bd9a54b5578f9d593ab5
ericmartinezr <eric.martinez@live.cl>
2015-11-18 01:42:53 -0300

    first commit

--- app.js
+++ app.js
@@ -10,22 +10,49 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
-var http_1 = require('angular2/http');
 var router_1 = require('angular2/router');
+var Main = (function () {
+    function Main() {
+    }
+    Main = __decorate([
+        angular2_1.Component({ template: 'Default child' }), 
+        __metadata('design:paramtypes', [])
+    ], Main);
+    return Main;
+})();
+var Index = (function () {
+    function Index() {
+    }
+    Index = __decorate([
+        angular2_1.Component({
+            template: 'Main! <router-outlet></router-outlet>',
+            directives: [router_1.ROUTER_DIRECTIVES]
+        }),
+        router_1.RouteConfig([
+            { path: 'main', component: Main, name: 'Main', useAsDefault: true }
+        ]), 
+        __metadata('design:paramtypes', [])
+    ], Index);
+    return Index;
+})();
 var App = (function () {
     function App() {
     }
     App = __decorate([
         angular2_1.Component({
             selector: 'app',
-            template: 'Hello world!'
-        }), 
+            template: 'Hello world! <router-outlet></router-outlet>',
+            directives: [router_1.ROUTER_DIRECTIVES]
+        }),
+        router_1.RouteConfig([
+            { path: '/', redirectTo: ['Index'] },
+            { path: '/index/...', component: Index, name: 'Index' }
+        ]), 
         __metadata('design:paramtypes', [])
     ], App);
     return App;
 })();
 angular2_1.bootstrap(App, [
-    http_1.HTTP_PROVIDERS,
     router_1.ROUTER_PROVIDERS,
     angular2_1.provide(router_1.ROUTER_PRIMARY_COMPONENT, { useValue: App }),
     angular2_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })
--- app.ts
+++ app.ts
@@ -1,11 +1,38 @@
 import {Component, bootstrap, provide} from 'angular2/angular2';
-import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {
+	RouteConfig,
+	ROUTER_PROVIDERS, 
+	ROUTER_DIRECTIVES,
+	ROUTER_PRIMARY_COMPONENT, 
+	LocationStrategy, 
+	HashLocationStrategy} from 'angular2/router';
+
+
+
+@Component({template : 'Default child'})
+class Main {}
+
+
+@Component({
+	template : 'Main! <router-outlet></router-outlet>',
+	directives : [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+	{path: 'main', component: Main, name : 'Main', useAsDefault: true}
+])
+class Index {}
+
+
 
 @Component({
 	selector : 'app',
-	template : 'Hello world!'
+	template : 'Hello world! <router-outlet></router-outlet>',
+	directives : [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+	{path : '/', redirectTo : ['Index']},
+	{path : '/index/...', component: Index, name : 'Index'}
+])
 class App {
 
 	constructor() {
@@ -14,7 +41,6 @@ class App {
 }
 
 bootstrap(App, [
-	HTTP_PROVIDERS,
 	ROUTER_PROVIDERS,
 	provide(ROUTER_PRIMARY_COMPONENT, {useValue: App}),
 	provide(LocationStrategy, {useClass: HashLocationStrategy})
